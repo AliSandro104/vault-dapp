fcd1c8857209b5ecf8ca6128b9f94a6b9e6b2a15
AliSandro104 <alisandro2000@hotmail.com>
2025-05-22 18:37:38 -0400

    fix failing unit tests

--- test/Vault.test.js
+++ test/Vault.test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Vault", function () {
   let Vault, vault, owner, addr1;
@@ -11,17 +12,30 @@ describe("Vault", function () {
   });
 
   it("should allow deposit and track balance", async function () {
-    const depositAmount = ethers.parseEther("1");
-    await vault.connect(addr1).deposit({ value: depositAmount });
-    const balance = await vault.balances(addr1.address);
-    expect(balance).to.equal(depositAmount);
+    const depositAmount = ethers.parseEther("0.1");
+    const unlockTime = Math.floor(Date.now() / 1000) + 60; // 1 minute in the future
+
+    await vault.connect(addr1).deposit(unlockTime, { value: depositAmount });
+
+    const deposits = await vault.getDeposits(addr1.address);
+    expect(deposits.length).to.equal(1);
+    expect(deposits[0].amount).to.equal(depositAmount);
   });
 
   it("should allow withdrawal of deposited funds", async function () {
-    const depositAmount = ethers.parseEther("2");
-    await vault.connect(addr1).deposit({ value: depositAmount });
-    await vault.connect(addr1).withdraw();
-    const balanceAfter = await vault.balances(addr1.address);
-    expect(balanceAfter).to.equal(0n);
+    const depositAmount = ethers.parseEther("0.2");
+    const unlockTime = Math.floor(Date.now() / 1000) + 10; // 10 seconds in the future
+
+    await vault.connect(addr1).deposit(unlockTime, { value: depositAmount });
+
+    // Increase time by 11 seconds to go past unlockTime
+    await network.provider.send("evm_increaseTime", [11]);
+    await network.provider.send("evm_mine"); // mine a block to apply the time change
+
+    await vault.connect(addr1).withdraw(0);
+
+    const deposits = await vault.getDeposits(addr1.address);
+    expect(deposits[0].amount).to.equal(0);
   });
+
 });
